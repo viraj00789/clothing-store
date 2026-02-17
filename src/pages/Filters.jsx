64b7128d01c8b8f4afb69b5f963e7a0188091f7a
viraj00789 <viraj.koradia@mindinventory.com>
2026-02-17 17:50:22 +0530

    fix: update layout prop in Filters component for improved animation behavior

--- src/pages/Filters.jsx
+++ src/pages/Filters.jsx
@@ -101,7 +101,7 @@ const Filters = () => {
           {width >= 1024 && !showSideBar && (
             <motion.div
               key="desktop-sidebar"
-              layout="size"
+              // layout="size"
               // initial={{ x: 40, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
               exit={{ x: -100, opacity: 0 }}
@@ -245,7 +245,7 @@ const Filters = () => {
                 {filteredProducts.map((product) => (
                   <motion.div
                     key={product.id}
-                    layout="size"
+                    layout="position"
                     // initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
                     exit={{ opacity: 0, y: -20 }}
