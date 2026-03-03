9bfc60df62d6c1f8c54e82f331e435294efa5ab3
viraj00789 <viraj.koradia@mindinventory.com>
2026-03-03 19:25:22 +0530

    feat: internationalize About Us component by extracting strings into locale files.

--- src/components/ui/home/AboutUs.jsx
+++ src/components/ui/home/AboutUs.jsx
@@ -1,35 +1,27 @@
+import { useTranslation } from "react-i18next";
+
 const AboutUs = () => {
+  const { t } = useTranslation();
   return (
     <div className="px-3.75 xl:px-12.5 space-y-3 lg:space-y-[27px] mt-6 lg:mt-[85px] rounded-[10px] pb-6 lg:pb-29">
       <h3 className="text-light-black font-normal text-2xl lg:text-4xl">
-        About Us
+        {t("about:title")}
       </h3>
       <div className="flex gap-0 lg:gap-7">
         <div className="w-full lg:w-1/2 space-y-3 lg:space-y-7">
-          <p className="text-respo">Business Name</p>
+          <p className="text-respo">{t("about:Business")}</p>
           <p className="font-normal text-md xl:text-2xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui vel
-            morbi cursus sed sodales molestie proin dictum gravida. Porttitor
-            maecenas tincidunt ipsum semper malesuada. In sapien feugiat laoreet
-            convallis eu sed. Sapien et montes, duis tempor euismod augue cras
-            eu eget. Risus suspendisse mauris ullamcorper felis a, quam. Lorem
-            ipsum dolor sit amet, consectetur adipiscing elit. Dui vel morbi
-            cursus sed sodales molestie proin dictum gravida. Porttitor maecenas
-            tincidunt ipsum semper malesuada. In sapien feugiat laoreet
-            convallis eu sed. Sapien et montes, duis tempor euismod augue cras
-            eu eget. Risus suspendisse mauris ullamcorper felis a, quam.Lorem
-            ipsum dolor sit amet, consectetur adipiscing elit. Dui vel morbi
-            cursus sed sodales molestie proin dictum gravida. Porttitor maecenas
+            {t("about:desc")}
           </p>
           <div className="space-y-2 lg:space-y-[15px]">
-            <p className="text-respo-24">Contact Information</p>
+            <p className="text-respo-24">{t("about:Contact")}</p>
             <p className="font-normal text-md xl:text-2xl">+91 1256378409</p>
             <p className="font-normal text-md xl:text-2xl">
               Someting@random.com
             </p>
           </div>
           <button className="border-2 border-mid-gray py-1 px-3 lg:px-8.75 lg:py-2.25 rounded-[10px] cursor-pointer font-normal text-lg hover:bg-black hover:text-white transition ease-in-out duration-300 max-w-[172px] whitespace-nowrap">
-            Directions
+            {t("about:Direction")}
           </button>
         </div>
         <div className="hidden lg:inline lg:w-1/2 min-h-[655px]">
